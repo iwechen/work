4fe9a6f61a11b28b9178c1f9763c82605f603292
iwechen <iwechen123@gmail.com>
2018-01-04 17:26:16 +0800

    完善美食杰平台多类型数据判断抓取问题

--- 005_MS_spider/ms_spider.py
+++ 005_MS_spider/ms_spider.py
@@ -2,11 +2,21 @@ import requests
 import re
 from lxml import etree
 import time
+import pymongo
+import gevent
+# from multiprocessing import Process
+import multiprocessing
+from gevent import monkey
+monkey.patch_all()
 
 class MSJ_Spider(object):
     '''美食杰爬虫项目'''
     def __init__(self):
         self.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3236.0 Safari/537.36"}
+        
+        self.client = pymongo.MongoClient(host='127.0.0.1',port=27017)
+        self.db = self.client['Items']
+        self.collection = self.db['msj_item']
 
     def send_request(self,url):
         response = requests.get(url = url,headers = self.headers).content.decode('utf8')
@@ -33,29 +43,62 @@ class MSJ_Spider(object):
     def collect_food_menu(self,response):
         food_dict = {}
         html = etree.HTML(response)
-        food_dict['title'] = html.xpath('//h1[@class="title"]/a/text()')[0]
-        food_dict['category'] = re.search(r'#(.*)#',html.xpath('//ul[@class="pathstlye1"]')[0].xpath('string(.)')).group(1)
-        food_dict['tags'] = [i[0].xpath('string(.)') for i in html.xpath('//div[@class="info1"]/dl/dt')]
-        food_dict['time'] = [i.xpath('string(.)') for i in html.xpath('//div[@class="info2"]/ul/li')]
-        food_dict['desc'] = html.xpath('//div[@class="materials"]/p/text()')[0]
-        # # 主料
-        main_sea = dict(zip([i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[1]/ul/li/div/h4/a')],[i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[1]/ul/li/div/h4/span')]))
-        # # 辅料
-        assist_sea = dict(zip([i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[2]/ul/li/h4/a')],[i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[2]/ul/li/span')]))
-        food_dict['sessioning'] = {'主料':main_sea,'辅料':assist_sea}
-        food_dict['link'] = html.xpath('//div[@class="cp_headerimg_w"]/img/@src')[0]
-        food_dict['method'] = [i.xpath('string(.)') for i in html.xpath('//div[@class="editnew edit"]/div/div/p[1]')]
-        food_dict['method_img'] = html.xpath('//div[@class="editnew edit"]/div/div/p[2]/img/@src')
-        food_dict['method_img'] = html.xpath('//div[@class="editnew edit"]/div/div/p[2]/img/@src')
-
-        print(food_dict)
-        return food_dict
+        try:
+            food_dict['title'] = html.xpath('//h1[@class="title"]/a/text()')
+            category = re.search(r'#(.*)#',html.xpath('//ul[@class="pathstlye1"]')[0].xpath('string(.)')).group(1)
+            food_dict['category'] = category
+            food_dict['tags'] = [i[0].xpath('string(.)') for i in html.xpath('//div[@class="info1"]/dl/dt')]
+            food_dict['time'] = [i.xpath('string(.)') for i in html.xpath('//div[@class="info2"]/ul/li')]
+            desc = html.xpath('//div[@class="materials"]/p/text()')
+            if desc ==[]:
+                food_dict['desc'] = ""
+            else:
+                food_dict['desc'] = desc[0]
+            # 主料
+            main_sea = dict(zip([i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[1]/ul/li/div/h4/a')],[i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[1]/ul/li/div/h4/span')]))
+            # 辅料
+            assist_sea = dict(zip([i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[2]/ul/li/h4/a')],[i.xpath('string(.)') for i in html.xpath('//div[@class="materials_box"]/div[2]/ul/li/span')]))
+            food_dict['sessioning'] = {'主料':main_sea,'辅料':assist_sea}
+            link = html.xpath('//div[@class="cp_headerimg_w"]/img/@src')[0]
+            food_dict['link'] = link
+            method = [i.xpath('string(.)') for i in html.xpath('//div[@class="editnew edit"]/div/div/p[1]')]
+            method_img = html.xpath('//div[@class="editnew edit"]/div/div/p[2]/img/@src')
+            if method==[]:
+                method = [i.xpath('string(.)') for i in html.xpath('//div[@class="edit edit_class_0 edit_class_13"]/p')]
+                method_img = html.xpath('//div[@class="edit edit_class_0 edit_class_13"]/p/img/@src')
+            food_dict['method'] = method
+            food_dict['method_img'] = method_img
+        except:
+            print('--------------------------error--------------------')
+            print(food_dict)
+            return False
+        else:
+            print(food_dict)
+            return food_dict
 
     def start_food(self,food_url):
+        food_li = []
         for url in food_url:
+            # url = 'http://www.meishij.net/zuofa/suanrongzhengjinzhengu.html'
+            error_link = {}
+            print(url)
             response = self.send_request(url)
+            food_dict = self.collect_food_menu(response)
+            if food_dict==False:
+                error_link['link'] = url
+                collection = self.db['msj_error_link']
+                collection.insert(error_link)
+                print('save_error_link_ok! ')
+            else:
+                food_li.append(food_dict)
+        self.save_to_mongo(food_li)
 
-            self.collect_food_menu(response)
+    def save_to_mongo(self,food_dict):
+        try:
+            self.collection.insert(food_dict)
+            print('successful')
+        except:
+            print('default！')
 
     def start_page(self,url_li):
         for url in url_li:
@@ -79,13 +122,19 @@ class MSJ_Spider(object):
         for cate_url in cate_li:
             response = self.send_request(cate_url)
             url_li = self.collect_food_url(response)
-            self.start_page(url_li)
+            # self.start_page(url_li)
+            # 创建协程爬取每个页面
+            g1 = gevent.spawn(self.start_page,url_li)
+            g1.join()
 
     def start_work(self):
         url = 'http://www.meishij.net/chufang/diy/'
         response = self.send_request(url)
         cate_li = self.collect_category(response) 
+        
         self.start_category(cate_li)
+        # p = multiprocessing.Process(target=self.start_category,args=(cate_li,))
+        # p.start()
 
     def main(self):
         self.start_work()
