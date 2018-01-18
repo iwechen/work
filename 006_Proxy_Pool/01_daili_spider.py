61567abb1e8b3cd73181895ebc89418cb9c64ccd
iwechen <iwechen123@gmail.com>
2018-01-18 17:45:23 +0800

    build proxy

--- 006_Proxy_Pool/01_daili_spider.py
+++ 006_Proxy_Pool/01_daili_spider.py
@@ -7,12 +7,12 @@ import pymongo
 class Proxy_Spider(object):
     def __init__(self):
         self.headers = {
-        'Accept': '*/*',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
+        # 'Accept': '*/*',
+        # 'Accept-Language': 'zh-CN,zh;q=0.8',
         'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-        'Hosts': 'hm.baidu.com',
-        'Referer': 'http://www.xicidaili.com/nn',
-        'Connection': 'keep-alive'
+        # 'Hosts': 'hm.baidu.com',
+        # 'Referer': 'http://www.xicidaili.com/nn',
+        # 'Connection': 'keep-alive'
         }
         self.client = pymongo.MongoClient(host = '127.0.0.1',port = 27017)
         self.db = self.client['Proxy']
@@ -22,42 +22,51 @@ class Proxy_Spider(object):
         '''获取代理'''
         # 获取页码范围
         i = 0
-        for page in range(22,1000):
+        for page in range(0,2000):
             print('第%d页'%page)
+            flag = True
+            while flag:
 
-            gip_li = [gip for gip in self.collection.find({'count':1})]
+                gip_li = [gip for gip in self.collection.find({'count':2})]
 
-            if i > len(gip_li)-1:
-                i = 0
+                if i > len(gip_li)-1:
+                    i = 0
 
-            pro_host = gip_li[i]['ip']
-            proxies = { "http":pro_host}
-            url = 'http://www.xicidaili.com/nn/' + str(page)
-            try:
-                response = requests.get(url = url, proxies = proxies, headers = self.headers).content.decode('utf-8')
-            except:
-                i+=1
-                continue
-            i+=1
-            ip_list = re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?(\d{2,6})", response, re.S)
-            print(ip_list)
-            
-            ip_li = [] 
-            for ip_tup in ip_list:
-                ip_dict = {}
-                ip = ip_tup[0] + ':' + ip_tup[1]
-                mip = [mip for mip in self.collection.find({'ip':ip})]
-                if len(mip) != 0:
-                    print('ip:%s已经存在!'%ip)
+                pro_host = gip_li[i]['ip']
+                print(pro_host)
+                proxies = { "http":pro_host}
+                url = 'https://proxy.coderbusy.com/classical/anonymous-type/highanonymous.aspx?page=' + str(page)
+                # url = 'http://www.bugng.com/'
+                # url = 'https://proxy.mimvp.com/free.php?proxy=in_tp&sort=&page='+str(page)
+                try:
+                    response = requests.get(url = url, proxies = proxies, headers = self.headers).content.decode('utf-8')
+                    print(response)
+                except:
+                    flag = True
+                    i+=1
                     continue
-                print(ip)
-                ip_dict['ip'] = ip
-                ip_dict['count'] = 0
-                ip_li.append(ip_dict)
+                else:
+                    flag = False
+                i+=1
+                ip_list = re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?(\d{2,6})", response, re.S)
+                print(ip_list)
+                
+                ip_li = [] 
+                for ip_tup in ip_list:
+                    ip_dict = {}
+                    ip = ip_tup[0] + ':' + ip_tup[1]
+                    mip = [mip for mip in self.collection.find({'ip':ip})]
+                    if len(mip) != 0:
+                        print('ip:%s已经存在!'%ip)
+                        continue
+                    print(ip)
+                    ip_dict['ip'] = ip
+                    ip_dict['count'] = 0
+                    ip_li.append(ip_dict)
 
-            print(ip_li)
-            self.save_to_mongo(ip_li)
-            # time.sleep(1)
+                print(ip_li)
+                self.save_to_mongo(ip_li)
+                # time.sleep(10)
             
     def save_to_mongo(self,ip_li):
         '''保存未验证ip到mongo'''
--- 006_Proxy_Pool/02_check_ip.py
+++ 006_Proxy_Pool/02_check_ip.py
@@ -2,10 +2,14 @@
 import pymongo
 import urllib
 import socket
-socket.setdefaulttimeout(4)
-import gevent
-from gevent import monkey
-monkey.patch_all()
+socket.setdefaulttimeout(5)
+
+import time
+
+import threading
+
+import os
+
 
 class Check_Ip(object):
     def __init__(self):
@@ -14,9 +18,13 @@ class Check_Ip(object):
         self.collection = self.db.proxy_daili
 
     def send_check(self,_id,count,proxy_temp):
+        # print('这是子进程，进程编号：%d' % os.getpid())
+        # time.sleep(10)
         url = "http://ip.chinaz.com/getip.aspx"
+        # url = "https://www.baidu.com/"
         try:
             res = urllib.urlopen(url,proxies=proxy_temp).read()
+            print(res)
         except Exception as e:
             count -=1 
             print e
@@ -27,7 +35,8 @@ class Check_Ip(object):
             self.collection.update({'_id': _id}, {"$set" :{"count": count}})
 
     def check_proxy(self):
-        for mip in self.collection.find({"count":0}):
+        # pool = multiprocessing.Pool(processes=10)
+        for mip in self.collection.find({"count":1}):
             ip = mip['ip']
             _id = mip['_id']
             count = mip['count']
@@ -35,8 +44,10 @@ class Check_Ip(object):
 
             proxy_temp = {"http":proxy_host}
             # self.send_check(_id,count,proxy_temp)
-            g1 = gevent.spawn(self.send_check,_id,count,proxy_temp)
-            g1.join()
+
+            t = threading.Thread(target = self.send_check,args=(_id,count,proxy_temp))
+            t.start()
+            t.join()
 
     def main(self):
         self.check_proxy()
